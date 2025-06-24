ae83c8556010a26d6da04eaadfcf6629ef55cf0f
Carlos Digaetano <digaetano.dev@gmail.com>
2025-06-23 21:41:14 -0400

    Created guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+          var random = new Random();
+          var favNumber = random.Next(1, 11);
+          
+          Console.WriteLine("Give me a number:");
+          int response = int.Parse(Console.ReadLine());
+
+          if (response < favNumber)
+
+          {
+           Console.WriteLine($"Too Low!\n Your guess: {response}");
+          }
+          else if (response > favNumber)
+
+          {
+              Console.WriteLine($"Too High!\n Your guess: {response}");
+          }
+          else
+          {
+              Console.WriteLine($"You guessed it!\nThe favorite number was {favNumber}");
+          }
+          
         }
     }
 }
